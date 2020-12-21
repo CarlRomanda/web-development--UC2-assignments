3f913dfe4233b832ae3f5854993dc2f0a386d857
Carl Genesis Romanda <75733763+CarlRomanda@users.noreply.github.com>
2020-12-22 00:01:08 +0800

    Add files via upload

---
+++ UC2 LO2.js
@@ -0,0 +1,66 @@
+var youLikeMeat = true;
+var myNum = 10;
+
+if (youLikeMeat){
+
+    document.write("you like meat");
+
+} else {
+
+    document.write("you like meat");
+}
+
+
+var myAge = 49;
+
+if (myAge > 30){
+
+    document.write("you are over 30!");
+
+} else if (myAge > 20){
+
+    document.write("you are over 20!");
+
+} else if (myAge > 10){
+
+    document.write("you are over 10!");
+
+} else {
+
+    document.write("you are not over 10!");
+
+}
+
+
+var myAge = 26;
+
+if (myAge < 18 || myAge > 30 || myAge --- 25){
+
+    document.write("you aint comming!");
+
+} else {
+
+    document.write("You can come, you cool dude");
+
+}
+
+var age = 5;
+
+while (age < 10) {
+
+    console.log("Your age is less than 10");
+    age++;
+
+}
+
+document/write("you are now over 10 v");
+
+var links = document.getElementByTagName("a");
+
+for (i = 1; i <= links.lenght; i++) {
+
+    console.log("this is link number" + i);
+
+}
+
+document.write("all links now looped");
